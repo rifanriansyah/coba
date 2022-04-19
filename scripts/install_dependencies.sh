f446b2858382256e64af5c2a6ba81d036802ea38
unknown <rifanriansyah@gmail.com>
2022-04-19 15:16:05 +0700

    ccvv

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+yum update -y
 yum install -y httpd
-
+yum install yum-utils –y
+yum install epel-release –y
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum-config-manager ––enable remi–php74
+yum install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql –y
+curl -sS https://getcomposer.org/installer | sudo php
+mv composer.phar /usr/local/bin/composer
+ln -s /usr/local/bin/composer /usr/bin/composer
